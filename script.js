044cf1d7bc163dbb9bbd17fe8ff2a60ae265a2ff
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-10-28 22:27:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var toggleButtom = document.getElementById("toggle-skills");
+var skills = document.getElementById("skills");
+toggleButtom.addEventListener('click', function () {
+    if (skills.style.display === "none") {
+        skills.style.display = 'block';
+    }
+    else {
+        skills.style.display = 'none';
+    }
+});
---
+++ script.ts
@@ -0,0 +1,10 @@
+const toggleButtom = document.getElementById("toggle-skills") as HTMLButtonElement;
+const skills = document.getElementById("skills") as HTMLElement
+
+toggleButtom.addEventListener('click' , ()=>{
+    if(skills.style.display === "none"){
+        skills.style.display = 'block'
+    }else{
+        skills.style.display = 'none'
+    }
+})
